85a81ea275d96ed9365e47457c375bdc5ce0282c
jayckaiser <jayckaiser@gmail.com>
2022-10-02 23:33:02 -0500

    Begin work on screenplay operation and associated yaml file.

--- renpy_analyzer/destinations/screenplay.py
+++ renpy_analyzer/destinations/screenplay.py
@@ -8,8 +8,14 @@ class Screenplay(Destination):
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.mode = 'screenplay'
 
-        self.allowed_configs.update([])
+        self.allowed_configs.update(["file_col", "screenplay_col", "justify", "line_sep"])
+
+        self.file_col = None
+        self.screenplay_col = None
+        self.justify = None
+        self.line_sep = None
 
 
     def compile(self):
@@ -18,7 +24,25 @@ class Screenplay(Destination):
         :return:
         """
         super().compile()
-        pass
+
+        # file_col: REQUIRED
+        self.error_handler.assert_key_exists_and_type_is(self.config, 'file_col', str)
+        self.file_col = self.config['file_col']
+
+        # screenplay_col: REQUIRED
+        self.error_handler.assert_key_exists_and_type_is(self.config, 'screenplay_col', str)
+        self.screenplay_col = self.config['screenplay_col']
+
+        # justify: OPTIONAL
+        if 'justify' in self.config:
+            self.error_handler.assert_key_type_is(self.config, 'justify', int)
+        self.justify = self.config.get('justify')
+
+        # line_sep: OPTIONAL
+        if 'line_sep' in self.config:
+            self.error_handler.assert_key_type_is(self.config, 'line_sep', str)
+        self.line_sep = self.config.get('line_sep', '\n\n')
+
 
 
     def execute(self):
